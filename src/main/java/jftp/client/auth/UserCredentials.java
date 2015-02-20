db1640b00e152bcdf7ada3653b47e22ef0ad4344
Josh Stark <josh.stark@travisperkins.co.uk>
2015-02-20 08:43:29 +0000

    Formatting

--- src/main/java/jftp/client/auth/UserCredentials.java
+++ src/main/java/jftp/client/auth/UserCredentials.java
@@ -2,23 +2,20 @@ package jftp.client.auth;
 
 public class UserCredentials {
 
-    private static final String ANONYMOUS_USER = "anonymous";
-    
     private String username;
     private String password;
 
-    public static final UserCredentials ANONYMOUS = new UserCredentials(ANONYMOUS_USER, "jftp@github.com");
-    
-    public UserCredentials(String username, String password)
-    {
+    public static final UserCredentials ANONYMOUS = new UserCredentials("anonymous", "jftp@github.com");
+
+    public UserCredentials(String username, String password) {
         this.username = username;
         this.password = password;
     }
-    
+
     public String getUsername() {
         return username;
     }
-    
+
     public String getPassword() {
         return password;
     }
